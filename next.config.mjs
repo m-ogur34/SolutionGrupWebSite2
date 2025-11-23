def470590fe33453bec870b85020055bc9c9adf7
Muhammed <muhammed.ogur@verion.com.tr>
2025-11-23 15:49:28 +0300

    Proje İsmi

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@
 const nextConfig = {
   output: "export",
   // GitHub Pages repository adınız buraya eklenmeli
-  basePath: "/solutiongrupwebsite2",
+  basePath: "/İsniCozelim",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   env: {
     // Resim yolları için kullanılan değişkeni de güncelliyoruz
-    NEXT_PUBLIC_BASE_PATH: "/solutiongrupwebsite2",
+    NEXT_PUBLIC_BASE_PATH: "/İsniCozelim",
   },
 };
 
